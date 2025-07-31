d7734dcb03c6ac33a12741f5cba4f8510e4e0326
Abaddon <donnie.l.baker@gmail.com>
2025-07-31 19:38:56 -0400

    Initial commit for Dashboard of Conan. The flask Dashboard for Conan the Botbarian. Skeleton

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Coming soon! Da shboard of Conan. The dashboard for Conan, The Botbarian."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
