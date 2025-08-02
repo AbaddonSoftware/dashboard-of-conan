ef5e91ef77e8cd8c0201d57cb5ea972dbd052b3f
Abaddon <donnie.l.baker@gmail.com>
2025-08-02 17:13:21 -0400

    style: added style from skeleton fix: improved structure

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
-from flask import Flask
+from app import create_app
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Coming soon! Da Shboard of Conan. The Dashboard for Conan, The Botbarian."
+app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
